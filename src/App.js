032d30f7b766fadb42065ed8f95a3466d2674389
cuongnc <codondemen1102@gmail.com>
2022-08-30 10:57:06 +0700

    splitting oit cart-item into it`s own component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import './categories.styles.scss';
+
+import CategoryItem from './components/category-item/category-item.component';
+
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+  },
+];
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ];
   return (
     <div className='categories-container'>
-      {categories.map(({ title, id }) => {
-        return (
-          <div className='category-container' key={id}>
-            <div className='background-image' />
-            <div className='category-body-container'>
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        );
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
       })}
     </div>
   );
