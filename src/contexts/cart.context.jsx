5e747c05f01e58b1db3f73cc499c3730c5fabc60
cuongnc <codondemen1102@gmail.com>
2022-09-08 09:47:37 +0700

    added cartItems features

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useState, createContext } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
@@ -22,16 +23,33 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   setCatItems: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
