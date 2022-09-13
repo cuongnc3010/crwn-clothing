a8c9811275cac827b1dff35e4c277360a450116b
cuongnc <codondemen1102@gmail.com>
2022-09-13 15:11:07 +0700

    fixed CSS clashing and improved solution

--- src/components/diretory/diretory.component.jsx
+++ src/components/diretory/diretory.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
 
 const Directory = ({ categories }) => {
   return (
     <div className='directory-container'>
       {categories.map((category) => {
-        return <CategoryItem key={category.id} category={category} />;
+        return <DirectoryItem key={category.id} category={category} />;
       })}
     </div>
   );
