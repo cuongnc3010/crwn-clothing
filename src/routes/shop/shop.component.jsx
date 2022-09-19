a18bc4d23d7533b5017d30a7b230122c6d9c78f6
cuongnc <codondemen1102@gmail.com>
2022-09-19 21:59:48 +0700

    moved categories context wrapping around shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
+import { CategoriesProvider } from '../../contexts/categories.context';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
 const Shop = () => {
   return (
-    <Routes>
-      <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
-    </Routes>
+    <CategoriesProvider>
+      <Routes>
+        <Route index element={<CategoriesPreview />} />
+        <Route path=':category' element={<Category />} />
+      </Routes>
+    </CategoriesProvider>
   );
 };
 export default Shop;
