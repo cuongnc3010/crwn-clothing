25af35386c7e7a000f6d8c6a8d4f0694e9dc41f3
cuongnc <codondemen1102@gmail.com>
2022-09-23 23:01:30 +0700

    adjust cart open intial state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { UserProvider } from './contexts/user.context';
 
 import App from './App';
 
 import { CartProvider } from './contexts/cart.context';
+<<<<<<< Updated upstream
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
+=======
+import { store } from './store/store';
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <Router>
       <UserProvider>
         <CartProvider>
@@ -20,6 +26,19 @@ root.render(
         </CartProvider>
       </UserProvider>
     </Router>
+=======
+    <Provider store={store}>
+      <Router>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </Router>
+    </Provider>
+>>>>>>> Stashed changes
   </React.StrictMode>
 );
 
